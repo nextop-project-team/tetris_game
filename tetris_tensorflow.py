1431e0e7c53fcdf3a95af54731345ae125bffcf2
KimMyoungSeop <49678602+KimMyoungSeop@users.noreply.github.com>
2019-05-30 19:54:26 +0900

    Update tetris_tensorflow.py
    5/30

--- tetris_tensorflow.py
+++ tetris_tensorflow.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 import numpy as np
 
 
+
 class ai:
-    def nextmove(self):
+    def train(self):
         xy = np.loadtxt('1.csv', delimiter=',', dtype=np.float32)
         x_data = xy[:,:-1]
         y_data = xy[:,[-1]] #회전 수
@@ -22,16 +23,35 @@ class ai:
         X = tf.placeholder(tf.float32, [None, 222])
         Y = tf.placeholder(tf.int32, [None, 1])  # 0 ~ 6
 
-        Y_one_hot = tf.one_hot(Y, 1)  # one hot
-        print("one_hot:", Y_one_hot)
-        Y_one_hot = tf.reshape(Y_one_hot, [-1, 10])
-        print("reshape one_hot:", Y_one_hot)
+        #Y_one_hot = tf.one_hot(Y, 1)  # one hot
+        #print("one_hot:", Y_one_hot)
+        #Y_one_hot = tf.reshape(Y_one_hot, [-1, 10])
+        #print("reshape one_hot:", Y_one_hot)
 
 
         W = tf.Variable(tf.random_normal([222,10]), name='weight1')
         b = tf.Variable(tf.random_normal([10]), name='bias')
 
-        logits = tf.matmul(X, W) + b
+        hypothesis = tf.matmul(X, W) + b
+
+        # Simplified cost/loss function
+        cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+        # Minimize
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
+        train = optimizer.minimize(cost)
+
+        # Launch the graph in a session.
+        sess = tf.Session()
+        # Initializes global variables in the graph.
+        sess.run(tf.global_variables_initializer())
+
+        for step in range(2001):
+            cost_val, hy_val, _ = sess.run(
+                [cost, hypothesis, train], feed_dict={X: x_data, Y: y_data})
+            if step % 10 == 0:
+                print(step, "Cost: ", cost_val, "\nPrediction:\n", hy_val)
+        '''logits = tf.matmul(X, W) + b
         hypothesis = tf.nn.softmax(logits)
 
         sess = tf.Session()
@@ -61,8 +81,12 @@ class ai:
             pred = sess.run(prediction, feed_dict={X: x_data})
             # y_data: (N,1) = flatten => (N, ) matches pred.shape
             for p, y in zip(pred, y_data.flatten()):
-                print("[{}] Prediction: {} True Y: {}".format(p == int(y), p, int(y)))
-            ai[3]=(0,p,0)
-        return ai
+                print("[{}] Prediction: {} True Y: {}".format(p == int(y), p, int(y)))'''
+
+
+    def test(self):
+        
+
+
 
 AI = ai()
